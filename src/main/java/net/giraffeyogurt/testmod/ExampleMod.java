fc4f4fa45d32cec66f6db5cc47286accb45a40b3
giraffeyogurt <mtaha2k11@outlook.com>
2025-01-10 17:41:04 +0500

    First commit test

--- src/main/java/net/giraffeyogurt/testmod/ExampleMod.java
+++ src/main/java/net/giraffeyogurt/testmod/ExampleMod.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+// Testing commits
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
